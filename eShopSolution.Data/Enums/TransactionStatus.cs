aa4ff28cddbef120f074cffdc48263598affeb7a
LeAnhKha <leanhkha2@gmail.com>
2022-10-23 20:48:47 +0700

    add Entity

---
+++ eShopSolution.Data/Enums/TransactionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aShopSolution.Data.Enums
+{
+    public enum TransactionStatus
+    {
+        Success,
+        Failed
+    }
+}
